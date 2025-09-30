b13f48f22d6c09eb74ba6269df82856aa3b691fa
DanaNava <dananavamontiel@gmail.com>
2025-09-29 19:39:03 -0600

    Agregue lo que llevo del menu

---
+++ proyecto.py
@@ -0,0 +1,114 @@
+# Para iniciar cualquier proyecto en pygame 
+import pygame
+import sys
+pygame.init()
+
+# Colorcitos
+negro = (   0,   0,   0)
+blanco = (255, 255, 255)
+verde = (  0, 255,   0)
+rojo = (255,   0,   0)
+azul = (  0,   0, 255)
+
+# Crear ventana
+screen = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption('Trash Hunters')
+
+#Imagen de fondo
+background = pygame.image.load("background1.png").convert()
+imagen_redimensionada = pygame.transform.scale(background, (1024, 768))
+start_img = pygame.image.load("start.png")
+start_hover = pygame.image.load("starth.png")
+tutorial = pygame.image.load("tutorial.png")
+tutorial_hover = pygame.image.load("tutorialh.png")
+configuracion = pygame.image.load("configuracion.png")
+configuracion_hover = pygame.image.load("configuracionh.png")
+creditos = pygame.image.load("creditos.png")
+creditos_hover = pygame.image.load("creditosh.png")
+
+
+# clase boton
+class Button():
+    def __init__(self, x, y, image, image_hover):
+        self.image = image
+        self.image_hover = image_hover
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self, pos_mouse):
+        if self.rect.collidepoint(pos_mouse):
+            screen.blit(self.image_hover, self.rect)
+        else:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+
+start_Button = Button(613, 512, start_img, start_hover)
+
+# Otro boton
+class Button2():
+    def __init__(self, x, y, image, image_hover):
+        self.image = image
+        self.image_hover = image_hover
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self, pos_mouse):
+        if self.rect.collidepoint(pos_mouse):
+            screen.blit(self.image_hover, self.rect)
+        else:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+
+start_Button2 = Button2(0, 0, tutorial, tutorial_hover)
+
+# Otro boton
+class Button3():
+    def __init__(self, x, y, image, image_hover):
+        self.image = image
+        self.image_hover = image_hover
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self, pos_mouse):
+        if self.rect.collidepoint(pos_mouse):
+            screen.blit(self.image_hover, self.rect)
+        else:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+
+start_Button3 = Button3(0, 0, configuracion, configuracion_hover)
+
+# Otro boton
+class Button4():
+    def __init__(self, x, y, image, image_hover):
+        self.image = image
+        self.image_hover = image_hover
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self, pos_mouse):
+        if self.rect.collidepoint(pos_mouse):
+            screen.blit(self.image_hover, self.rect)
+        else:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+
+start_Button4 = Button4(0, 0, creditos, creditos_hover)
+
+
+# Crear el bucle del juego
+while True:
+    pos_mouse = pygame.mouse.get_pos()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    # Poner fondo en la pantalla
+    screen.blit(imagen_redimensionada, (0, 0))
+
+    # Inicia zona de diseños y dibujos
+
+    start_Button.draw(pos_mouse)
+    start_Button2.draw(pos_mouse)
+    start_Button3.draw(pos_mouse)
+    start_Button4.draw(pos_mouse)
+    
+    # Actualizar pantalla
+    pygame.display.flip()
