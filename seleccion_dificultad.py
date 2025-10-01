e98d05e9cf35627ae2d6b9631497c7739f9c4624
aibarra9-ux <aibarra9@ucol.mx>
2025-09-30 18:39:15 -0600

    Actualizo seleccion_dificultad.py

--- seleccion_dificultad.py
+++ seleccion_dificultad.py
@@ -4,6 +4,7 @@ class MiInterfaz:
     def __init__(self, screen):
         self.screen = screen
         self.running = True
+        self.cambio = None
         # Aquí cargas tus imágenes, botones, fuentes, etc.
         self.fondo = pygame.image.load("assets_PI/interfaces/eleguir_dificultad/fondo/fondo_interfaz_elegir_dificultad.png").convert()  
        
@@ -26,10 +27,10 @@ class MiInterfaz:
             mouse_pos = event.pos
             for i, boton in enumerate(self.botones):
                 if boton["rect"].collidepoint(mouse_pos):
-                    if i == 0:
-                        print("Seleccionaste Fácil")
-                    elif i == 1:
-                        print("Seleccionaste Medio")
+                   if i == 0:
+                    self.cambio = "facil"
+                   elif i == 1:
+                    self.cambio = "medio"
 
     def update(self):
         # Aquí actualizas animaciones, colores, etc.
@@ -44,5 +45,8 @@ class MiInterfaz:
                 self.screen.blit(boton["hover"], boton["rect"].topleft)
             else:
                 self.screen.blit(boton["normal"], boton["rect"].topleft)
+
+
+
 
 
