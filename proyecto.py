8255193bb7a2adcaba0caf38ae28f6d5ad19ab1e
Dana Paola Nava Montiel <dananavamontiel@gmail.com>
2025-09-30 21:46:04 -0600

    Menu principal primera version casifinalizada, faltan interfaces conectadas

--- proyecto.py
+++ proyecto.py
@@ -41,7 +41,10 @@ class Button():
         else:
             screen.blit(self.image, (self.rect.x, self.rect.y))
 
-start_Button = Button(403, 315, start_img, start_hover)
+    def clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos)
+
+start_button = Button(403, 315, start_img, start_hover)
 
 # Otro boton
 class Button2():
@@ -57,7 +60,10 @@ class Button2():
         else:
             screen.blit(self.image, (self.rect.x, self.rect.y))
 
-start_Button2 = Button2(404, 537, tutorial, tutorial_hover)
+    def clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos)
+
+tutorial_button = Button2(404, 537, tutorial, tutorial_hover)
 
 # Otro boton
 class Button3():
@@ -73,7 +79,10 @@ class Button3():
         else:
             screen.blit(self.image, (self.rect.x, self.rect.y))
 
-start_Button3 = Button3(10, 666, configuracion, configuracion_hover)
+    def clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos)
+
+configuracion_button = Button3(10, 666, configuracion, configuracion_hover)
 
 # Otro boton
 class Button4():
@@ -89,7 +98,10 @@ class Button4():
         else:
             screen.blit(self.image, (self.rect.x, self.rect.y))
 
-start_Button4 = Button4(925, 676, creditos, creditos_hover)
+    def clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos)
+
+creditos_button = Button4(925, 676, creditos, creditos_hover)
 
 
 # Crear el bucle del juego
@@ -100,15 +112,32 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
+    # Condicion que detectara que se clickeo un boton y actualizara su estado
+    if start_button.clicked(event):
+        #! Iniciar nueva interfaz aqui
+        print("Inicia el jueguito aqui")
+
+    if tutorial_button.clicked(event):
+        #! Iniciar nueva interfaz aqui
+        print("Iniciar tutorial aqui")
+
+    if configuracion_button.clicked(event):
+        #! Iniciar nueva interfaz aqui
+        print("Abrir configuracion aqui")
+
+    if creditos_button.clicked(event):
+        #! Iniciar nueva interfaz aqui
+        print("Abrir creditos aqui")
+
     # Poner fondo en la pantalla
     screen.blit(imagen_redimensionada, (0, 0))
 
     # Inicia zona de diseños y dibujos
 
-    start_Button.draw(pos_mouse)
-    start_Button2.draw(pos_mouse)
-    start_Button3.draw(pos_mouse)
-    start_Button4.draw(pos_mouse)
+    start_button.draw(pos_mouse)
+    tutorial_button.draw(pos_mouse)
+    configuracion_button.draw(pos_mouse)
+    creditos_button.draw(pos_mouse)
     
     # Actualizar pantalla
     pygame.display.flip()
